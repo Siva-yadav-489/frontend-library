c2ef0773e27aa9afb3b37094cdd6f4509c3a00e5
Siva Kumar Yadav <skumaryadav489@gmail.com>
2025-06-06 16:16:47 +0530

    fixed image errors

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'store.storeimages.cdn-apple.com',
+      'as-images.apple.com'
+    ],
+  },
+}
+
+module.exports = nextConfig 
